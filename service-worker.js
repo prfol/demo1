aa1d795c9acdaba1e8dc87bc35c73fb3c358a716
prfol <jrrm1611@gmail.com>
2025-07-20 19:08:45 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_NAME = 'my-pwa-cache-v2';
 
 self.addEventListener('install', (event) => {
     console.log('Service Worker: Evento de instalación');
